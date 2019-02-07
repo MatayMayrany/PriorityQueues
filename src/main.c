572492a454cf63d61d77a9e474d0d90339df8711
Louis <me@louiscb.com>
2019-02-07 10:55:29 +0100

    Added inputs and outputs for user

--- src/main.c
+++ src/main.c
@@ -7,7 +7,18 @@ int main() {
     //First variable:
     //   For Ready testing is the number of times you want to test the enqueue dequeue operations
     //   For Event testing is the total size you want to the list to iterate
-    test(1100, HEAP_EVENT_CODE, 1000);
+    printf("Priority Queue Experiments\n");
+    int code;
+    printf("Choose the type of priority queue implementation:\n 1 - List Ready\n 2 - Heap Ready\n 3 - List Event\n 4 - Heap Event\n ");
+    scanf("%d",&code);
+    int size;
+    printf("Specify the size of the queue: ");
+    scanf("%d",&size);
+    int iterations;
+    printf("Specify the number of iterations: ");
+    scanf("%d",&iterations);
+
+    test(iterations, code, size);
 
     return 0;
 }
